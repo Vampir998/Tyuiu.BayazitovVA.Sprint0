b4fcd10dd51dc6bdb1f755dcad5a9a7f51d7afc4
Vampir <vadim.bayazitov.08@gmail.com>
2026-09-14 12:24:04 +0500

    Task1

---
+++ Tyuiu.BayazitovVA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.BayazitovVA.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            string name = "Вадим";
+            int age = 18;
+
+            Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет.");
+            Console.ReadKey();
+        }
+    }
+}
