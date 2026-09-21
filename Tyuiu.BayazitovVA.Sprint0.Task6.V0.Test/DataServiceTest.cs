0bfcb2b6fe4d26e53d673af7882cb57b45bd78f3
Vampir <vadim.bayazitov.08@gmail.com>
2026-09-21 16:54:01 +0500

    Task6

---
+++ Tyuiu.BayazitovVA.Sprint0.Task6.V0.Test/DataServiceTest.cs
@@ -0,0 +1,30 @@
+﻿namespace Tyuiu.BayazitovVA.Sprint0.Task6.V0.Test
+{
+    using Tyuiu.BayazitovVA.Sprint0.Task6.V0.Lib;
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckAdditionArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.AdditionArray(numbers);
+            Assert.AreEqual(15, res);
+        }
+        [TestMethod]
+        public void CheckSubstractionArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.SubstractionArray(numbers);
+            Assert.AreEqual(-15, res);
+        }
+
+        [TestMethod]
+        public void CheckMultArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.MultiplicationArray(numbers);
+            Assert.AreEqual(120, res);
+        }
+    }
+}
---
+++ Tyuiu.BayazitovVA.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Tyuiu.BayazitovVA.Sprint0.Task6.V0;
+using Tyuiu.BayazitovVA.Sprint0.Task6.V0.Lib;
+class Programm
+{
+    
+    static void Main(string[] args)
+    {
+        int[] numsArray = new int[] { 1, 2, 3, 4, 5 };
+
+        Console.WriteLine("Сумма элементов массива = " + DataService.AdditionArray(numsArray));
+
+        Console.WriteLine("Разность элементов массива = " + DataService.SubstractionArray(numsArray));
+
+        Console.WriteLine("Произведение элементов массива = " + DataService.MultiplicationArray(numsArray));
+
+        Console.ReadKey();
+    }
+}
