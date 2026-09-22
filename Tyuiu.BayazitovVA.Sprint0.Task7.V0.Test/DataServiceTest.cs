fb50ba8ceb79943ece49926dbc16d17b3ebb8b3a
Vampir <vadim.bayazitov.08@gmail.com>
2026-09-22 13:39:33 +0500

    Task7

---
+++ Tyuiu.BayazitovVA.Sprint0.Task7.V0.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿namespace Tyuiu.BayazitovVA.Sprint0.Task7.V0.Test
+{
+    using Tyuiu.BayazitovVA.Sprint0.Task7.V0.Lib;
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckAdditionArrayValid()
+        {
+            int[] nums1 = new int[] { 1, 1, 1, 1, 1 };
+            int[] nums2 = new int[] { 1, 1, 1, 1, 1 };
+            int[] nums3 = new int[] { 2, 2, 2, 2, 2 };
+
+            int[] res = DataService.AdditionArrays(nums1, nums2);
+            CollectionAssert.AreEqual(nums3, res);
+        }
+    }
+}
